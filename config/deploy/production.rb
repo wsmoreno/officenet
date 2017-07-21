52b89954312d45e76a7304dc265407d31321e720
Wilson Moreno <wilson_moreno@1stslp.com>
2017-07-21 11:50:21 +0800

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+ENV["RAILS_ENV"] = "development"
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -6,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+
 role :app, %w{hq.unitrade.local}
 role :web, %w{hq.unitrade.local}
 role :db,  %w{hq.unitrade.local}
